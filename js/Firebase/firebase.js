97a502541387b1ce4efd47ddbcdac1c34d311cc1
Yvonne1009 <chiu60917@gmail.com>
2024-01-23 00:56:43 +0800

    0123-12:56

--- js/Firebase/firebase.js
+++ js/Firebase/firebase.js
@@ -1,13 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
-
-const firebaseConfig = {
+// Initialize Firebase
+var config = {
   apiKey: "AIzaSyCkMa2jz6kDse8j3BSsXYKru8LYA_Sb2M0",
   authDomain: "leisurepace-a3d51.firebaseapp.com",
   databaseURL: "https://leisurepace-a3d51-default-rtdb.firebaseio.com",
@@ -17,3 +9,4 @@ const firebaseConfig = {
   appId: "1:955257567601:web:2e06661d84707065fe9ed0",
   measurementId: "G-MHZ967Y676",
 };
+firebase.initializeApp(config);
