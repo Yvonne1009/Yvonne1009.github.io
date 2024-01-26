f7842949dc58f99cd2766877624f6c08e5f2a0c3
Yvonne1009 <chiu60917@gmail.com>
2024-01-26 20:36:06 +0800

    0126 登入問題還有

--- js/login.js
+++ js/login.js
@@ -1,3 +1,44 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+    // Get idToken
+    user.getIdToken().then(function (token) {
+      console.log(token);
+    });
+    if (user != null) {
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "User : " + email_id;
+    }
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
+});
+
+function login() {
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      window.alert("Error : " + errorMessage);
+
+      // ...
+    });
+}
+
 // FB 登入
 function fb_login() {
   const provider = new firebase.auth.FacebookAuthProvider();
@@ -52,4 +93,28 @@ function google_login() {
       // ...
       console.log(errorMessage);
     });
+}
+
+// 訪客登入
+function phone_login() {
+  window.location.href = "phone.html";
+}
+
+function changeEmail() {
+  // 自動偵測瀏覽器語言
+  firebase.auth().useDeviceLanguage();
+  // 修改 Email
+  var user = firebase.auth().currentUser;
+  user
+    .updateEmail("andy6804tw@gmail.com")
+    .then(function () {
+      // Update successful.
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
+}
+
+function logout() {
+  firebase.auth().signOut();
 }
