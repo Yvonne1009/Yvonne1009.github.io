b67b9e0f25ecf621fc79363cd704e6b2fbc8746d
Yvonne1009 <chiu60917@gmail.com>
2024-05-13 05:20:19 +0800

    000

--- firebase/public/init-firebase.js
+++ firebase/public/init-firebase.js
@@ -1,12 +1,15 @@
 // TODO: Replace the following with your app's Firebase project configuration
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCkMa2jz6kDse8j3BSsXYKru8LYA_Sb2M0",
-    authDomain: "leisurepace-a3d51.firebaseapp.com",
-    databaseURL: "https://leisurepace-a3d51-default-rtdb.firebaseio.com/",
-    projectId: "leisurepace-a3d51",
-    storageBucket: "gs://leisurepace-a3d51.appspot.com/",
+  apiKey: "AIzaSyCkMa2jz6kDse8j3BSsXYKru8LYA_Sb2M0",
+  authDomain: "leisurepace-a3d51.firebaseapp.com",
+  databaseURL: "https://leisurepace-a3d51-default-rtdb.firebaseio.com/",
+  projectId: "leisurepace-a3d51",
+  storageBucket: "gs://leisurepace-a3d51.appspot.com/",
 };
-  
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// 获取对数据库的引用
+const database = firebase.database();
