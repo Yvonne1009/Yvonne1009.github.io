549572481b31aa23e65eae1e22290a61294c6c07
terao499 <62597993+terao499@users.noreply.github.com>
2024-09-04 15:28:41 +0800

    Update del_acc.js

--- firebase/public/js/del_acc.js
+++ firebase/public/js/del_acc.js
@@ -1,16 +1,4 @@
 // del_acc.js
-var firebaseConfig = {
-  apiKey: "AIzaSyCkMa2jz6kDse8j3BSsXYKru8LYA_Sb2M0",
-  authDomain: "leisurepace-a3d51.firebaseapp.com",
-  databaseURL: "https://leisurepace-a3d51-default-rtdb.firebaseio.com/",
-  projectId: "leisurepace-a3d51",
-  storageBucket: "gs://leisurepace-a3d51.appspot.com/",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// Firebase Authentication reference
-const auth = firebase.auth();
 
 document.getElementById('confirm_delete_btn').addEventListener('click', function() {
     const user = auth.currentUser; // Get the current user
