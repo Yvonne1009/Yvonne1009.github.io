f41d971a6e7780966a1e86cae960d07a52a0d67f
terao499 <62597993+terao499@users.noreply.github.com>
2024-10-03 16:37:18 +0800

    1003

--- firebase/public/js/init-firebase.js
+++ firebase/public/js/init-firebase.js
@@ -1,4 +1,8 @@
 // TODO: Replace the following with your app's Firebase project configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 
 var firebaseConfig = {
   apiKey: "AIzaSyCkMa2jz6kDse8j3BSsXYKru8LYA_Sb2M0",
@@ -13,5 +17,10 @@ firebase.initializeApp(firebaseConfig);
 
 // 获取对数据库的引用
 const database = firebase.database();
-//對帳號的引用
-const auth = firebase.auth();
+// 初始化 Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// 將 auth 和 db 輸出，供其他檔案使用
+export { auth, db };
