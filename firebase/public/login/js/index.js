0c4fea7813d1a3df8f114769ebbc3f0a84c83e08
Yvonne1009 <chiu60917@gmail.com>
2024-10-07 05:27:48 +0800

    logout

--- firebase/public/login/js/index.js
+++ firebase/public/login/js/index.js
@@ -1,4 +1,4 @@
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // 使用者已登入。
     document.getElementById("user_div").style.display = "block";
@@ -9,61 +9,62 @@ firebase.auth().onAuthStateChanged(function(user) {
     user.getIdToken().then(function (token) {
       console.log(token);
     });
-    if(user != null){
-
+    if (user != null) {
       var email_id = user.email;
       document.getElementById("user_para").innerHTML = "User : " + email_id;
-
     }
-
   } else {
     // 沒有使用者登入。
 
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
-function login(){
+function login() {
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // 在這裡處理錯誤。
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      // 在這裡處理錯誤。
+      var errorCode = error.code;
+      var errorMessage = error.message;
 
-    window.alert("Error : " + errorMessage);
+      window.alert("Error : " + errorMessage);
 
-    // ...
-  });
-
+      // ...
+    });
 }
 
 // FB 登入
 function fb_login() {
-
   const provider = new firebase.auth.FacebookAuthProvider();
   // 自動偵測瀏覽器語言
   firebase.auth().useDeviceLanguage();
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    //這將為您提供 Facebook 存取權令牌。您可以使用它來存取 Facebook API。
-    var token = result.credential.accessToken;
-    // 登入的用戶資訊。
-    var user = result.user;
-    // ...
-  }).catch(function (error) {
-    // 在這裡處理錯誤。
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // 使用的使用者帳戶的電子郵件。
-    var email = error.email;
-    // 使用的 firebase.auth.AuthCredential 類型。
-    var credential = error.credential;
-    // ...
-    console.log(errorMessage);
-  });
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      //這將為您提供 Facebook 存取權令牌。您可以使用它來存取 Facebook API。
+      var token = result.credential.accessToken;
+      // 登入的用戶資訊。
+      var user = result.user;
+      // ...
+    })
+    .catch(function (error) {
+      // 在這裡處理錯誤。
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // 使用的使用者帳戶的電子郵件。
+      var email = error.email;
+      // 使用的 firebase.auth.AuthCredential 類型。
+      var credential = error.credential;
+      // ...
+      console.log(errorMessage);
+    });
 }
 
 // google 登入
@@ -71,23 +72,27 @@ function google_login() {
   const provider = new firebase.auth.GoogleAuthProvider();
   // 自動偵測瀏覽器語言
   firebase.auth().useDeviceLanguage();
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    console.log(errorMessage);
-  });
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(errorMessage);
+    });
 }
 
 // 訪客登入
@@ -100,15 +105,27 @@ function changeEmail() {
   firebase.auth().useDeviceLanguage();
   // 修改 Email
   var user = firebase.auth().currentUser;
-  user.updateEmail("andy6804tw@gmail.com").then(function () {
-    // Update successful.
-  }).catch(function (error) {
-    // An error happened.
-  });
+  user
+    .updateEmail("andy6804tw@gmail.com")
+    .then(function () {
+      // Update successful.
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
 }
 
-
-function logout(){
-  firebase.auth().signOut();
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      // Sign-out successful.
+      alert("You have successfully logged out.");
+      window.location.href = "index1.html"; // 導向到 index1.html
+    })
+    .catch(function (error) {
+      // An error happened.
+      console.error("Error signing out: ", error);
+    });
 }
-
