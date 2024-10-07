9a9b7e6f80a67d1ebd366ff8b154a9b6c2726f70
Yvonne1009 <chiu60917@gmail.com>
2024-10-08 01:00:25 +0800

    填表單頁面及轉跳畫面

--- firebase/public/login/js/register.js
+++ firebase/public/login/js/register.js
@@ -10,18 +10,14 @@ firebase.auth().onAuthStateChanged(function (user) {
       console.log(token);
     });
     if (user != null) {
-
       var email_id = user.email;
       document.getElementById("user_para").innerHTML = "User : " + email_id;
-
     }
-
   } else {
     // 沒有使用者登入。
 
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
   }
 });
 
@@ -41,13 +37,24 @@ function login() {
     return; // 停止继续执行
   }
 
-  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function (error) {
-    // 在这里处理错误。.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(errorMessage);
-    // ...
-  });
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(userEmail, userPass)
+    .then(function (userCredential) {
+      // 註冊成功
+      var user = userCredential.user;
+      console.log("User registered: ", user);
+
+      // 跳轉到 fill in.html
+      window.location.href =
+        "https://yvonne1009.github.io/firebase/public/login/fill%20in.html";
+    })
+    .catch(function (error) {
+      // 在这里处理错误。
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
 }
 
 // 验证电子邮件格式的函数
@@ -57,6 +64,17 @@ function validateEmail(email) {
 }
 
 function logout() {
-  firebase.auth().signOut();
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      // Sign-out successful.
+      alert("You have successfully logged out.");
+      window.location.href =
+        "https://yvonne1009.github.io/firebase/public/index1.html";
+    })
+    .catch(function (error) {
+      // An error happened.
+      console.error("Error signing out: ", error);
+    });
 }
-
